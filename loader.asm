c53c735d9f1eb6b921b68c849eb09df85acc99f2
larva <nguyenthanhcong12gtm1@gmail.com>
2023-05-03 16:31:32 +0700

    Check long mode is supported.

--- loader.asm
+++ loader.asm
@@ -2,6 +2,23 @@
 [ORG 0x7e00]
 
 start:
+	mov [DriveID],dl
+      	
+	mov eax,0x80000000
+      	cpuid
+       	cmp eax,0x80000001
+  	jb NotSupport
+	
+	mov eax,0x80000001
+	cpuid
+	
+	test edx,(1<<29)
+	jz NotSupport
+        
+	test edx,(1<<26)
+        jz NotSupport
+
+
         mov ah,0x13
         mov al,1
         mov bx,0xa
@@ -10,9 +27,11 @@ start:
         mov cx,MessageLen
         int 0x10
 
+NotSupport:
 End:
         hlt
         jmp End
 
-Message:        db "Loader is running."
+DriveID: 	db 0
+Message:        db "Loader is running"
 MessageLen:     equ $-Message
