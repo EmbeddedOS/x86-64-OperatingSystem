6620128c7638cc27a59522d7057d115489f100c2
larva <nguyenthanhcong12gtm1@gmail.com>
2023-07-14 19:31:17 +0700

    Edit tab to spaces.

--- boot.asm
+++ boot.asm
@@ -19,79 +19,79 @@
 ;      |     MBR code      |
 ;      |-------------------| 0x7C00 <--SP
 ;      |       STACK       |           ||
-;	   |                   |           ||
+;      |                   |           ||
 ;      |                   |           \/
 ;      |-------------------| 0
 ; Here we go!
 
 [BITS 16]
-[ORG 0x7C00]	; The boot code start at address 0x7C00 and ascending.
+[ORG 0x7C00]    ; The boot code start at address 0x7C00 and ascending.
 
 Start:
-	; 1. Clear segment registers.
-	xor ax, ax
-	mov ds, ax
-	mov es, ax
-	mov ss, ax
+    ; 1. Clear segment registers.
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
 
-	; 2. Stack pointer start at address 0x7c00 and grows downwards.
-	mov sp, 0x7C00
+    ; 2. Stack pointer start at address 0x7c00 and grows downwards.
+    mov sp, 0x7C00
 
-	; 3. Test Disk Extension Service.
+    ; 3. Test Disk Extension Service.
 TestDiskExtension:
-	mov [DriveID], dl
-	mov ah, 0x41		; Use INT 13 Extensions - INSTALLATION CHECK service.
-	mov bx, 0x55AA
-	int 0x13			; Call the Disk Service.
-	jc NotSupport		; CF - Carry Flag is set on error or clear on success.
-	cmp bx, 0xAA55		; bx = 0xAA55 if installed.
-	jne NotSupport
+    mov [DriveID], dl
+    mov ah, 0x41        ; Use INT 13 Extensions - INSTALLATION CHECK service.
+    mov bx, 0x55AA
+    int 0x13            ; Call the Disk Service.
+    jc NotSupport       ; CF - Carry Flag is set on error or clear on success.
+    cmp bx, 0xAA55      ; bx = 0xAA55 if installed.
+    jne NotSupport
 
-	; 4. Load the loader.
+    ; 4. Load the loader.
 LoadLoader:
-	mov si, ReadPacket			; Load the packet address to si.
-	mov word[si], 0x10			; Packet size is 16 bytes.
-	mov word[si + 2], 0x05		; We we load 5 sectors which is enough space for
-								; our loader.
-	mov word[si + 4], 0x7E00	; Offset which we want to read loader file.
-	mov word[si + 6], 0x00		; Segment, the logical memory to load the file
-								; is: 0x00 * 0x10 + 0x7E00 = 0x7E00
-	mov dword[si + 8], 0x01		; 32 bit low address of LBA.
-	mov dword[si + 12], 0x00	; 32 bit high address of LBA.
-								; We will start at sector 2 but set 1 to LBA
-								; Because the LBA is zero-based address.
+    mov si, ReadPacket          ; Load the packet address to si.
+    mov word[si], 0x10          ; Packet size is 16 bytes.
+    mov word[si + 2], 0x05      ; We we load 5 sectors which is enough space for
+                                ; our loader.
+    mov word[si + 4], 0x7E00    ; Offset which we want to read loader file.
+    mov word[si + 6], 0x00      ; Segment, the logical memory to load the file
+                                ; is: 0x00 * 0x10 + 0x7E00 = 0x7E00
+    mov dword[si + 8], 0x01     ; 32 bit low address of LBA.
+    mov dword[si + 12], 0x00    ; 32 bit high address of LBA.
+                                ; We will start at sector 2 but set 1 to LBA
+                                ; Because the LBA is zero-based address.
 
-	mov dl, [DriveID]			; DriveID param.
-	mov ah, 0x42				; Use INT 13 Extensions - EXTENDED READ service.
-	int 0x13					; Call the Disk Service.
-	jc ReadError				; Carry flag will be set if error.
+    mov dl, [DriveID]           ; DriveID param.
+    mov ah, 0x42                ; Use INT 13 Extensions - EXTENDED READ service.
+    int 0x13                    ; Call the Disk Service.
+    jc ReadError                ; Carry flag will be set if error.
 
-	; 5. Loader code has been loaded to physical memory, jump to loader code and 
-	; transfer control to it.
-	mov dl, [DriveID]
-	jmp 0x7E00
+    ; 5. Loader code has been loaded to physical memory, jump to loader code and 
+    ; transfer control to it.
+    mov dl, [DriveID]
+    jmp 0x7E00
 
 NotSupport:
 ReadError:
-	mov ah, 0x13
-	mov al, 1
-	mov bx, 0xA
-	xor dx, dx
-	mov bp, Message
-	mov cx, MessageLen
-	int 0x10
+    mov ah, 0x13
+    mov al, 1
+    mov bx, 0xA
+    xor dx, dx
+    mov bp, Message
+    mov cx, MessageLen
+    int 0x10
 
 ; Halt CPU if we encounter some errors.
 End:
-	hlt
-	jmp End
+    hlt
+    jmp End
 
-DriveID:	db 0
-Message:	db "An error is occured when booting system."
-MessageLen:		equ $-Message
+DriveID:            db 0
+Message:            db "An error is occured when booting system."
+MessageLen:         equ $-Message
 
 # Disk Address Packet Structure.
-ReadPacket:		times 16 db 0
+ReadPacket:         times 16 db 0
 
 ; Clear memory from current address to address at byte 446th (0x1BE).
 times (0x1BE-($-$$)) db 0
@@ -100,12 +100,12 @@ times (0x1BE-($-$$)) db 0
 ; BIOS will try to find the valid partition entries. We want the BIOS treat our
 ; image as a hard disk and boot from them, so we need to define these entries.
 ; The first  partition entry:
-db 0x80					; Boot indicator, 0x80 means boot-able partion.
-db 0, 2, 0				; Starting of CHS value (Cylinder, Head, Sector).
-db 0xF0					; Type of sector.
-db 0xFF, 0xFF, 0xFF		; Ending of CHS value (Cylinder, Head, Sector).
-dd 1					; Starting sector.
-dd (20*16*63 - 1)		; Size of our disk: 10MB.
+db 0x80                     ; Boot indicator, 0x80 means boot-able partion.
+db 0, 2, 0                  ; Starting of CHS value (Cylinder, Head, Sector).
+db 0xF0                     ; Type of sector.
+db 0xFF, 0xFF, 0xFF         ; Ending of CHS value (Cylinder, Head, Sector).
+dd 1                        ; Starting sector.
+dd (20*16*63 - 1)           ; Size of our disk: 10MB.
 
 ; Other entries are set to 0.
 times (16*3) db 0
