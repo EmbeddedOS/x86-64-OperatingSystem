53400dc9ea839a60e43804db002edc950c9d19ee
larva <nguyenthanhcong12gtm1@gmail.com>
2023-07-28 22:26:39 +0700

    Fix error assert expression wrong.

--- assert.h
+++ assert.h
@@ -2,7 +2,7 @@
 
 #include <stdint.h>
 
-#define ASSERT(exp) do {if (!exp) { error_point(__FILE__, __LINE__);} } while(0)
+#define ASSERT(exp) do {if (!(exp)) { error_point(__FILE__, __LINE__);} } while(0)
 
 void error_point(const char *file, uint64_t line);
 void panic(const char *msg);
