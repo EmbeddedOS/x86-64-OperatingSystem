268fe47639ad59d3457173547585319175ef4862
larva <nguyenthanhcong12gtm1@gmail.com>
2023-08-08 18:59:22 +0700

    Implement BIOS parameter block, FAT16 file system.

--- boot.asm
+++ boot.asm
@@ -22,11 +22,121 @@
 ;      |                   |           ||
 ;      |                   |           \/
 ;      |-------------------| 0
-; Here we go!
+;
+; The Boot Sector structure:
+; |   Part    |Offset|Size|                     Description                    |
+; |-----------|------|----|----------------------------------------------------|
+; |   Code    |0x0000| 3  | Code to jump to the bootstrap code.                |
+; |-----------|------|----|----------------------------------------------------|
+; | OS name   |0x0003| 8  | Oem Id - Name of the formatting OS.                |
+; |-----------|------|----|----------------------------------------------------|
+; |    BIOS   |0x000B| 2  | Bytes per sector. This value is the number of bytes|
+; | parameter |      |    | in each physical sector. The allowed values are:   |
+; |   Block   |      |    | 512, 1024, 2048 or 4096 bytes.                     |
+; |           |0x000D| 1  | Sectors per Cluster. The allowed values are: 1, 2, |
+; |           |      |    | 4, 8, 16, 32 or 128.                               |
+; |           |0x000E| 2  | Reserved sectors from the start of the volume.     |
+; |           |      |    | Since the reserved region always contain the boot  |
+; |           |      |    | sector a zero value in this field is not allowed.  |
+; |           |      |    | The usual setting of this value is 1. The value is |
+; |           |      |    | used to calculate the location for the first sector|
+; |           |      |    | containing the FAT.                                |
+; |           |0x0010| 1  | Number of FAT copies - Usual 2 copies are used to  |
+; |           |      |    | prevent data loss. The usage of two copies are to  |
+; |           |      |    | prevent data loss if one or part of one FAT copy is|
+; |           |      |    | corrupted.                                         |
+; |           |0x0011| 2  | Number of possible root entries - 512 entries are  |
+; |           |      |    | recommended.                                       |
+; |           |0x0013| 2  | Small number of sectors - Used when volume size is |
+; |           |      |    | less than 32 Mb. This field states the total number|
+; |           |      |    | of sectors in the volume.                          |
+; |           |0x0015| 1  | Media Descriptor:                                  |
+; |           |      |    |  Hex    Capacity        Physical Format            |
+; |           |      |    | + F0    2.88 MB     3.5-inch, 2-sided, 36-sector   |
+; |           |      |    | + F0    1.44 MB     3.5-inch, 2-sided, 18-sector   |
+; |           |      |    | + F8       ?                Fixed disk             |
+; |           |      |    | + F9    720 KB      3.5-inch, 2-sided, 9-sector    |
+; |           |      |    | + F9    1.2 MB     5.25-inch, 2-sided, 15-sector   |
+; |           |      |    | + FA       ?                    ?                  |
+; |           |      |    | + FB       ?                    ?                  |
+; |           |      |    | + FC    180 KB     5.25-inch, 1-sided, 9-sector    |
+; |           |      |    | + FD    360 KB     5.25-inch, 2-sided, 9-sector    |
+; |           |      |    | + FE    160 KB	   5.25-inch, 1-sided, 8-sector    |
+; |           |      |    | + FF    320 KB     5.25-inch, 2-sided, 9-sector    |
+; |           |0x0016| 2  | Sector per FAT. This is the number of sectors      |
+; |           |      |    | occupied by one copy of the FAT.                   |
+; |           |0x0018| 2  | Sectors per Track. This value is used when the     |
+; |           |      |    | volume is on a media which have a geometry, that is|
+; |           |      |    | when the LBA number is broken down into a          |
+; |           |      |    | Cylinder-Head-Sector address. This field represents|
+; |           |      |    | the multiple of the max.                           |
+; |           |0x001A| 2  | Number of Heads. This value is used when the volume|
+; |           |      |    | is on a media which have a geometry, that is when  |
+; |           |      |    | the LBA number is broken down into a               |
+; |           |      |    | Cylinder-Head-Sector address. This field represents|
+; |           |      |    | the Head value used when the volume was formatted. |
+; |           |0x001C| 4  | Hidden Sectors. When the volume is on a media that |
+; |           |      |    | is partitioned, this value contains the number of  |
+; |           |      |    | sectors preceeding the first sector of the volume. |
+; |           |0x0020| 4  | Large number of sectors - Used when volume size is |
+; |           |      |    | greater than 32 Mb.                                |
+; |-----------|------|----|----------------------------------------------------|
+; | Extended  |0x0024| 1  | Drive Number. Used by some bootstrap code, fx.     |
+; |    BIOS   |      |    | MS-DOS. This is the int 13h drive number of the    |
+; | parameter |      |    | drive. The value 0x00 is used for the first floppy |
+; |   Block   |      |    | drive and the value 0x80 is used for the first     |
+; |           |      |    | harddrive.                                         |
+; |           |0x0025| 1  | Reserved. Is used by Windows NT to decide if it    |
+; |           |      |    | shall check disk integrity.                        |
+; |           |0x0026| 1  | Extended Boot Signature. Indicates that the next   |
+; |           |      |    | three fields are available.                        |
+; |           |0x0027| 4  | Volume Serial Number. This value is a 32 bit random|
+; |           |      |    | number, which, combined with the volume label,     |
+; |           |      |    | makes is possible to track removable media and     |
+; |           |      |    | check if the correct one is inserted.              |
+; |           |0x002B| 11 | Volume Label. Should be the same as in the root    |
+; |           |      |    | directory.                                         |
+; |           |0x0036| 8  | File System Type. The string should be 'FAT16 '.   |
+; |-----------|------|----|----------------------------------------------------|
+; |   Code    |0x003E|448 | Bootstrap code. May schrink in the future.         |
+; |-----------|------|----|----------------------------------------------------|
+; | Signature |0x00FE| 2  | Boot sector signature. This is the AA55h signature.|
+; |-----------|------|----|----------------------------------------------------|
+
+
+
+
 
 [BITS 16]
 [ORG 0x7C00]    ; The boot code start at address 0x7C00 and ascending.
 
+; Region 1: Code to jump to the bootstrap code.
+jmp short Start
+nop
+
+; Region 2: BIOS Parameter Block.
+OEMIdetifier db     'LARVAOS '
+BytesPerSector      dw 0x200
+SectorsPerCluster   db 0x80
+ReservedSectors     dw 200      ; 200 sector for our kernel.
+FATcopies           db 0x02
+RootDirEntries      dw 0x40
+NumSectors          dw 0x00
+MediaType           db 0xF8
+SectorsPerFAT       dw 0x100
+SectorsPerTrack     dw 0x20
+NumberOfHeads       dw 0x40
+HiddenSectors       dd 0x00
+SectorsBig          dd 0x773594
+
+; Region 3: Extended BIOS Parameter Block.
+DriveNumber             db 0x80
+Signature               db 0x29
+VolumeID                dd 0xD105
+VolumeIDString          db 'LARVAOS    '
+SystemIDString          db 'FAT16   '
+
+; Region 4: Bootstrap code.
 Start:
     ; 1. Clear segment registers.
     xor ax, ax
@@ -51,8 +161,8 @@ TestDiskExtension:
 LoadLoader:
     mov si, ReadPacket          ; Load the packet address to si.
     mov word[si], 0x10          ; Packet size is 16 bytes.
-    mov word[si + 2], 0x05      ; We we load 5 sectors which is enough space for
-                                ; our loader.
+    mov word[si + 2], 0x15      ; We we load 15 sectors which is enough space
+                                ; for our loader.
     mov word[si + 4], 0x7E00    ; Offset which we want to read loader file.
     mov word[si + 6], 0x00      ; Segment, the logical memory to load the file
                                 ; is: 0x00 * 0x10 + 0x7E00 = 0x7E00
@@ -101,15 +211,15 @@ times (0x1BE-($-$$)) db 0
 ; image as a hard disk and boot from them, so we need to define these entries.
 ; The first  partition entry:
 db 0x80                     ; Boot indicator, 0x80 means boot-able partion.
-db 0, 2, 0                  ; Starting of CHS value (Cylinder, Head, Sector).
-db 0xF0                     ; Type of sector.
-db 0xFF, 0xFF, 0xFF         ; Ending of CHS value (Cylinder, Head, Sector).
-dd 1                        ; Starting sector.
-dd (20*16*63 - 1)           ; Size of our disk: 10MB.
+db 1, 1, 0                  ; Starting of CHS value (Cylinder, Head, Sector).
+db 0x06                     ; Type of sector.
+db 0x0F, 0x3F, 0xCA         ; Ending of CHS value (Cylinder, Head, Sector).
+dd 0x3F                     ; Starting sector.
+dd 0x31F11                  ; Size of our disk: 10MB.
 
 ; Other entries are set to 0.
 times (16*3) db 0
 
-; Final two bytes are signature of the boot sector - magic numbers.
+; Region 5: Boot sector signature.
 db 0x55
 db 0xAA
